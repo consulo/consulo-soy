d10d6b1304807c22e3c96cd025678cfb84543191
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-04-18 13:05:33 +0300

    drop ide api deps

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,28 @@
  * @since 29/01/2023
  */
 module consulo.soy {
-  requires consulo.ide.api;
+  requires consulo.code.editor.api;
+  requires consulo.color.scheme.api;
+  requires consulo.component.api;
+  requires consulo.configurable.api;
+  requires consulo.datacontext.api;
+  requires consulo.document.api;
+  requires consulo.file.editor.api;
+  requires consulo.index.io;
+  requires consulo.language.api;
+  requires consulo.language.code.style.api;
+  requires consulo.language.code.style.ui.api;
+  requires consulo.language.editor.api;
+  requires consulo.language.impl;
+  requires consulo.localize.api;
+  requires consulo.logging.api;
+  requires consulo.project.api;
+  requires consulo.ui.api;
+  requires consulo.util.collection;
+  requires consulo.util.io;
+  requires consulo.util.lang;
+  requires consulo.virtual.file.system.api;
+
   requires com.intellij.xml.api;
   requires com.intellij.xml.html.api;
   requires com.intellij.xml;
